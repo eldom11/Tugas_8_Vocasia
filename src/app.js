0d9eecab81c0d8d45aaef3335449e3b48ab7e24a
eldom11 <2210631170065@student.unsika.ac.id>
2024-11-05 19:32:59 +0700

    feat: menambahkan cors ke API

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
 const express = require('express')
 const routes = require('./routes')
 const connectDB = require('./config/mongodb')
+const cors = require('cors')
 const app = express()
-// const cors = require('cors')
 
 require('dotenv').config({ path: './src/.env' })
-
-// app.use(cors())
 
 const port = process.env.PORT
 
 connectDB()
 
-app.use(express.json());
-app.use("/api/v1",routes)
+app.use(cors({
+  origin: 'http://mosulganteng.com'
+})) 
 
+app.use(express.json())
+app.use("/api/v1", routes)
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server berjalan di port ${port}`)
 })
 
 app.use(function (err, req, res, next) {
@@ -25,3 +26,4 @@ app.use(function (err, req, res, next) {
 })
 
 app.use(express.static('public/cover_book'))
+app.use(express.static('public/author_image'))
